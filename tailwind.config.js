49045f2eb7b03024caee94d0587d6fb619caf3ce
balajiprojects049 <balajiprojects049@gmail.com>
2026-06-28 16:37:30 +0530

    Reapply "Update Landing page with YouTube video and custom bounce animation"
    This reverts commit 435fa9220d1e45506bda0f6fc1251c00d69d90f4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,10 +67,21 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "bounce-x": {
+          "0%, 100%": {
+            transform: "translateX(0)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)"
+          },
+          "50%": {
+            transform: "translateX(25%)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)"
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "bounce-x": "bounce-x 1s infinite",
       },
     },
   },
